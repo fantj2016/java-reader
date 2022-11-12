3f0fe5e6ab8875b8d4f4ab54e7df74bec92af876
fantj <844072586@qq.com>
2022-11-12 22:29:24 +0800

    对象创建过程&字节码指令

---
+++ a-code/src/Entry.java
@@ -0,0 +1,12 @@
+public class Entry {
+    private int total = 10;
+
+    public int getTotal() {
+        return total;
+    }
+
+    public Entry setTotal(int total) {
+        this.total = total;
+        return this;
+    }
+}
